d915adaab8343e71fbceeeb5fe2ba78a8dfcc6b1
Aditya Kumar Gupta <adityakumargupta45321@gmail.com>
2026-02-09 17:49:17 +0530

    9th feb , 2026

---
+++ src/main/java/com/ecom/Shopping_cart/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecom.Shopping_cart.repository;
+import com.ecom.Shopping_cart.model.UserDtls;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<UserDtls, Integer> {
+    public UserDtls findByEmail(String email);
+
+    public List<UserDtls> findByRole(String role);
+
+    public UserDtls findByResetToken(String token);
+}
